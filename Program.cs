1e8cac0f8fefaa6d7edc187f7c98d737c8c7f053
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-03 18:26:09 +0100

    better report of assertion failure

--- Program.cs
+++ Program.cs
@@ -1,19 +1,37 @@
-﻿Console.WriteLine("Start");
-var totalMs = 0L;
-
-typeof(Aoc2024).GetMethods(BindingFlags.Static | BindingFlags.Public)
-    .Where(m => m.Name.StartsWith("Day"))
-    .Where(m => m.Name != "DayTemplate")
-    .OrderByDescending(m => m.Name.Length)
-    .ThenByDescending(m => m.Name)
-    .ToList()
-    .ForEach(m => 
+﻿try
+{
+    Console.WriteLine("Start");
+    var totalMs = 0L;
+    
+    typeof(Aoc2024).GetMethods(BindingFlags.Static | BindingFlags.Public)
+        .Where(m => m.Name.StartsWith("Day"))
+        .Where(m => m.Name != "DayTemplate")
+        .OrderByDescending(m => m.Name.Length)
+        .ThenByDescending(m => m.Name)
+        .ToList()
+        .ForEach(m => 
+        {
+            Console.Write(m.Name.PadLeft(5));
+            var timer = Stopwatch.StartNew(); m.Invoke(null, null); timer.Stop();
+            Console.WriteLine($" {timer.ElapsedMilliseconds} ms");
+            totalMs += timer.ElapsedMilliseconds;
+        });
+    
+    Console.WriteLine($"Total: {totalMs:#,#} ms");
+    Console.WriteLine("Done!");
+}
+catch (Exception e)
+{
+    while (e.InnerException is not null)
     {
-        Console.Write(m.Name.PadLeft(5));
-        var timer = Stopwatch.StartNew(); m.Invoke(null, null); timer.Stop();
-        Console.WriteLine($" {timer.ElapsedMilliseconds} ms");
-        totalMs += timer.ElapsedMilliseconds;
-    });
+        e = e.InnerException;
+    }
 
-Console.WriteLine($"Total: {totalMs:#,#} ms");
-Console.WriteLine("Done!");
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(e.Message);
+    Console.ResetColor();
+    if (e is not FluentAssertions.Execution.AssertionFailedException)
+    {
+        Console.WriteLine(e.StackTrace);
+    }
+}
