908cf5e4c92b23193f2d73866dacd5f13e8d3690
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-03 06:54:30 +0100

    Day 3 part 2

--- Day3.cs
+++ Day3.cs
@@ -15,26 +15,58 @@ static partial class Aoc2024
                 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
                 """.ToLines();
             Part1(input).Should().Be(161);
-            Part2(input).Should().Be(0);
+            input = 
+                """
+                xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+                """.ToLines();
+            Part2(input).Should().Be(48);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
             Part1(input).Should().Be(161289189L);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(83595109L);
         }
 
-        long Part1(string[] lines)
+        long Part1(string[] lines) => RunInstructions(lines, ignoreDo: true);
+        long Part2(string[] lines) => RunInstructions(lines, ignoreDo: false);
+
+        long RunInstructions(string[] lines, bool ignoreDo)
         {
-            Regex mulRegex = new(@"mul\((\d+),(\d+)\)");
-            long sum = 0;
+            var mulRegex = MulRegex();
+            var sum = 0L;
+            var doSum = true;
 
             foreach (var line in lines)
             {
                 var matches = mulRegex.Matches(line);
                 foreach (Match match in matches)
                 {
+                    if (ignoreDo && match.Groups[0].Value.StartsWith(DONT))
+                    {
+                        continue;
+                    }
+                    if (ignoreDo && match.Groups[0].Value.StartsWith(DO))
+                    {
+                        continue;
+                    }
+                    if (!ignoreDo && match.Groups[0].Value.StartsWith(DONT))
+                    {
+                        doSum = false;
+                        continue;
+                    }
+                    else if (!ignoreDo && match.Groups[0].Value.StartsWith(DO))
+                    {
+                        doSum = true;
+                        continue;
+                    }
+
+                    if (!doSum)
+                    {
+                        continue;
+                    }
+
                     var a = match.Groups[1].Value.ToLong();
                     var b = match.Groups[2].Value.ToLong();
                     sum += a * b;
@@ -43,6 +75,11 @@ static partial class Aoc2024
 
             return sum;
         }
-        int Part2(string[] lines) => 0;
     }
+
+    const string DO = "do";
+    const string DONT = "don't";
+
+    [GeneratedRegex(@$"mul\((\d+),(\d+)\)|{DO}\(\)|{DONT}\(\)")]
+    private static partial Regex MulRegex();
 }
