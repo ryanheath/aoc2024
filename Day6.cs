34bd95632ec5905a893adbaabcb1064f3264bc14
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-06 10:48:08 +0100

    Day 6 part 2

--- Day6.cs
+++ Day6.cs
@@ -1,4 +1,6 @@
-﻿static partial class Aoc2024
+﻿using Microsoft.VisualBasic;
+
+static partial class Aoc2024
 {
     public static void Day6()
     {
@@ -22,83 +24,114 @@
                 ......#...
                 """.ToLines();
             Part1(input).Should().Be(41);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(6);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
             Part1(input).Should().Be(4722);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(1602);
         }
 
         int Part1(string[] lines)
         {
             var (obstructions, guard, dim) = Parse(lines);
 
-            return GetVisited(obstructions, guard, dim).Count;
+            return GetVisited(obstructions, guard, dim)!.Count;
+        }
 
-            static HashSet<(int x, int y)> GetVisited(HashSet<(int x, int y)> obstructions, (int x, int y, Direction d) guard, (int maxX, int maxY) dim)
+        int Part2(string[] lines)
+        {
+            var (obstructions, guard, dim) = Parse(lines);
+
+            var path = GetVisited(obstructions, guard, dim)!;
+
+            var loops = 0;
+
+            // just brute force each position to find a loop
+            foreach (var (x, y) in path)
             {
-                var (x, y, d) = guard;
-                HashSet<(int x, int y)> visited = [];
-                visited.Add((x, y));
+                if (obstructions.Contains((x, y))) continue;
+                if (guard.x == x && guard.y == y) continue;
 
-                while (Walk());
+                // add a obstruction at this position
+                obstructions.Add((x, y));
 
-                return visited;
-
-                bool Walk()
+                if (GetVisited(obstructions, guard, dim) is null)
                 {
-                    var (nextX, nextY) = NextPosition();
-                    if (OutOfBounds(nextX, nextY)) return false;
-                    if (obstructions.Contains((nextX, nextY)))
-                    {
-                        Rotate90();
-                    }
-                    else
-                    {
-                        x = nextX;
-                        y = nextY;
-                        visited.Add((x, y));
-                    }
-                    return true;
-
-                    (int x, int y) NextPosition() => d switch
-                    {
-                        Direction.N => (x, y - 1),
-                        Direction.E => (x + 1, y),
-                        Direction.S => (x, y + 1),
-                        Direction.W => (x - 1, y),
-                        _ => throw new InvalidOperationException()
-                    };
-
-                    bool OutOfBounds(int nextX, int nextY) => nextX < 0 || nextX >= dim.maxX || nextY < 0 || nextY >= dim.maxY;
-
-                    void Rotate90() => d = (Direction)(((int)d + 1) % 4);
+                    loops++;
                 }
+
+                // remove the obstruction
+                obstructions.Remove((x, y));
             }
 
-            static (HashSet<(int x, int y)> obstructions, (int x, int y, Direction d) guard, (int maxX, int maxY) dim) Parse(string[] lines)
+            return loops;
+        }
+
+        static HashSet<(int x, int y)>? GetVisited(HashSet<(int x, int y)> obstructions, (int x, int y, Direction d) guard, (int maxX, int maxY) dim)
+        {
+            var (x, y, d) = guard;
+            HashSet<(int x, int y, Direction d)>? visited = [];
+            visited.Add((x, y, d));
+
+            while (Walk());
+
+            // strip the direction from the visited positions
+            return visited?.Select(v => (v.x, v.y)).ToHashSet();
+
+            bool Walk()
             {
-                HashSet<(int x, int y)> obstructions = [];
-                (int x, int y, Direction d) guard = (0, 0, Direction.N);
-
-                for (var y = 0; y < lines.Length; y++)
-                for (var x = 0; x < lines[y].Length; x++)
+                var (nextX, nextY) = NextPosition();
+                if (OutOfBounds(nextX, nextY)) return false;
+                if (obstructions.Contains((nextX, nextY)))
                 {
-                    switch (lines[y][x])
-                    {
-                        case '#': obstructions.Add((x, y)); break;
-                        case '^': guard = (x, y, Direction.N); break;
-                    }   
+                    Rotate90();
                 }
+                else
+                {
+                    if (Seen(nextX, nextY, d)) { visited = null; return false; };
+                    x = nextX;
+                    y = nextY;
+                }
+                visited.Add((x, y, d));
+                return true;
 
-                return (obstructions, guard, (lines[0].Length, lines.Length));
+                (int x, int y) NextPosition() => d switch
+                {
+                    Direction.N => (x, y - 1),
+                    Direction.E => (x + 1, y),
+                    Direction.S => (x, y + 1),
+                    Direction.W => (x - 1, y),
+                    _ => throw new InvalidOperationException()
+                };
+
+                bool OutOfBounds(int x, int y) => x < 0 || x >= dim.maxX || y < 0 || y >= dim.maxY;
+
+                void Rotate90() => d = (Direction)(((int)d + 1) % 4);
+
+                bool Seen(int x, int y, Direction d) => visited.Contains((x, y, d));
             }
         }
 
-        int Part2(string[] lines) => 0;
+        static (HashSet<(int x, int y)> obstructions, (int x, int y, Direction d) guard, (int maxX, int maxY) dim) Parse(string[] lines)
+        {
+            HashSet<(int x, int y)> obstructions = [];
+            (int x, int y, Direction d) guard = (0, 0, Direction.N);
+
+            for (var y = 0; y < lines.Length; y++)
+            for (var x = 0; x < lines[y].Length; x++)
+            {
+                switch (lines[y][x])
+                {
+                    case '#': obstructions.Add((x, y)); break;
+                    case '^': guard = (x, y, Direction.N); break;
+                }   
+            }
+
+            return (obstructions, guard, (lines[0].Length, lines.Length));
+        }
     }
 
     enum Direction { N, E, S, W }
