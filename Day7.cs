2da6079719e98255b95446389c6a2afe94e65272
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-08 12:16:21 +0100

    Revisited Day 7

--- Day7.cs
+++ Day7.cs
@@ -31,17 +31,21 @@
             Part2(input).Should().Be(38322057216320);
         }
 
-        long Part1(string[] lines) => TestLines(lines, Multiply, Add);
-        long Part2(string[] lines) => TestLines(lines, Multiply, Add, Concat);
+        long Part1(string[] lines) => TestLines(lines, Div, Sub);
+        long Part2(string[] lines) => TestLines(lines, Div, Sub, Spl);
 
         static long TestLines(string[] lines, params Func<long, long, long>[] operations)
             => Parse(lines)
             .Where(x => IsValid((x.testValue, x.values), operations))
             .Sum(x => x.testValue);
 
-        static long Multiply(long a, long b) => a * b;
-        static long Add(long a, long b) => a + b;
-        static long Concat(long a, long b) => a * PowLogApprox(b) + b;
+        static long Div(long a, long b)
+        {
+            var (div, mod) = Math.DivRem(a, b);
+            return mod == 0 ? div : -1;
+        }
+        static long Sub(long a, long b) => a - b;
+        static long Spl(long a, long b) => Div(a - b, PowLogApprox(b));
         static long PowLogApprox(long b) => b switch
         {
             < 10 => 10,
@@ -52,10 +56,13 @@
 
         static bool IsValid((long testValue, long[] values) line, params ReadOnlySpan<Func<long, long, long>> operations)
         {
+            // process backwards to reduce the search space and speed up runtime
+            // see explanation at https://www.reddit.com/r/adventofcode/comments/1h8l3z5/comment/m0yo4ba
+
             var resultValues = new Stack<(long result, int nextIndex)>();
             var lastIndex = line.values.Length - 1;
 
-            resultValues.Push((line.values[0], 1));
+            resultValues.Push((line.testValue, line.values.Length - 1));
 
             while (resultValues.Count > 0)
             {
@@ -66,13 +73,13 @@
                 {
                     var nextResult = operation(result, nextValue);
 
-                    if (nextIndex == lastIndex && nextResult == line.testValue)
+                    if (nextIndex == 0 && nextResult == 0)
                     {
                         return true;
                     }
-                    else if (nextResult <= line.testValue && nextIndex < lastIndex)
+                    else if (nextResult > 0 && nextIndex > 0)
                     {
-                        resultValues.Push((nextResult, nextIndex + 1));
+                        resultValues.Push((nextResult, nextIndex - 1));
                     }
                 }
             }
