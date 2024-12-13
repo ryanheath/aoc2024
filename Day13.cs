31706e2c9b77c54d59bd86bf648cc4e649a8c65a
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-13 21:04:08 +0100

    Day 13 part 2

--- Day13.cs
+++ Day13.cs
@@ -27,39 +27,44 @@
                 Prize: X=18641, Y=10279
                 """.ToLines();
             Part1(input).Should().Be(480);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(875318608908);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
             Part1(input).Should().Be(37128);
-            Part2(input).Should().Be(0);
+            Part2(input).Should().Be(74914228471331);
         }
 
-        int Part1(string[] lines) => Parse(lines).Select(m => CheapestPrize(m, 3, 1)).Where(x => x != int.MaxValue).Sum();
-        int Part2(string[] lines) => 0;
+        long Part1(string[] lines) => SumPrizes(Parse(lines));
+        long Part2(string[] lines) => SumPrizes(Parse(lines).Select(m => m with { px = m.px + 10000000000000, py = m.py + 10000000000000 }));
 
-        static int CheapestPrize((int ax, int ay, int bx, int by, int px, int py) machine, int costA, int costB)
+        static long SumPrizes(IEnumerable<(long ax, long ay, long bx, long by, long px, long py)> machines)
+            => machines.Select(Prize).Sum(x => x != long.MaxValue ? x : 0);
+
+        static long Prize((long ax, long ay, long bx, long by, long px, long py) machine)
         {
             var (ax, ay, bx, by, px, py) = machine;
-            var maxATimes = px / ax;
 
-            for (var i = maxATimes; i > 0; i -= 1)
+            // a = (5400*22)-(67*8400) / ((22*34) + (-94*67))
+            var d1 = py * bx - by * px;
+            var d2 = bx * ay - ax * by;
+            var a = Math.DivRem(d1, d2, out var remainder);
+
+            if (remainder != 0)
             {
-                var timesA = i;
-                var timesB = Math.DivRem(px - timesA * ax, bx, out var remainder);
-                if (remainder != 0) continue;
-                if (timesA * ay + timesB * by != py) continue;
-                return timesA * costA + timesB * costB;
+                return long.MaxValue;
             }
 
-            return int.MaxValue;
+            var b = (px - ax * a) / bx;
+
+            return 3 * a + 1 * b;
         }
 
-        static (int ax, int ay, int bx, int by, int px, int py)[] Parse(string[] lines)
+        static (long ax, long ay, long bx, long by, long px, long py)[] Parse(string[] lines)
         {
-            var list = new List<(int ax, int ay, int bx, int by, int px, int py)>();
+            var list = new List<(long ax, long ay, long bx, long by, long px, long py)>();
 
             for (var i = 0; i < lines.Length; i++)
             {
@@ -68,10 +73,10 @@
                 var (px, py) = ParseLine("Prize: ");
                 list.Add((ax, ay, bx, by, px, py));
 
-                (int x, int y) ParseLine(string prefix)
+                (long x, long y) ParseLine(string prefix)
                 {
                     var parts = lines[i++][prefix.Length..].Split(", ");
-                    return (parts[0]["X_".Length..].ToInt(), parts[1]["Y_".Length..].ToInt());
+                    return (parts[0]["X_".Length..].ToLong(), parts[1]["Y_".Length..].ToLong());
                 }
             }
 
