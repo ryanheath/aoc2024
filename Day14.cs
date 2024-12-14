15c87aa2e06701ebe2f36b73c4a88725b62f9d9c
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-14 10:18:34 +0100

    Day 14 part 2

--- Day14.cs
+++ Day14.cs
@@ -24,18 +24,17 @@
                 p=9,5 v=-3,-3
                 """.ToLines();
             Part1(input, 11, 7).Should().Be(12);
-            Part2(input).Should().Be(0);
         }
 
         void Compute()
         {
             var input = File.ReadAllLines($"{day.ToLowerInvariant()}.txt");
-            Part1(input, 101, 103).Should().Be(0);
-            Part2(input).Should().Be(0);
+            Part1(input, 101, 103).Should().Be(231782040);
+            Part2(input, 101, 103).Should().Be(6475);
         }
 
         int Part1(string[] lines, int w, int h) => SafetyFactor(Parse(lines), w, h, 100);
-        int Part2(string[] lines) => 0;
+        int Part2(string[] lines, int w, int h) => FindChristmasTree(Parse(lines), w, h);
 
         static int SafetyFactor((int x, int y, int vx, int vy)[] robots, int w, int h, int s)
         {
@@ -55,6 +54,45 @@
             }
 
             return q1 * q2 * q3 * q4;
+        }
+
+        static int FindChristmasTree((int x, int y, int vx, int vy)[] robots, int w, int h)
+        {
+            var s = 0;
+
+            while (true)
+            { 
+                for (var i = 0; i < robots.Length; i++)
+                {
+                    var r = robots[i];
+                    robots[i] = r with { x = PositiveMod(r.x + r.vx, w), y = PositiveMod(r.y + r.vy, h) };
+                }
+                s++;
+
+                // just try to find a block of 3x3 robots next to each other
+                robots = robots.OrderBy(r => r.y).ThenBy(r => r.x).ToArray();
+                for (var i = 0; i < robots.Length; i++)
+                {
+                    var r1 = robots[i];
+                    var r2 = robots.Skip(i+1).Any(r => r1.x + 1 == r.x && r.y == r1.y + 0);
+                    if (!r2) continue;
+                    var r3 = robots.Skip(i+1).Any(r => r1.x + 2 == r.x && r.y == r1.y + 0);
+                    if (!r3) continue;
+                    var r4 = robots.Skip(i+1).Any(r => r1.x + 0 == r.x && r.y == r1.y + 1);
+                    if (!r4) continue;
+                    var r5 = robots.Skip(i+1).Any(r => r1.x + 1 == r.x && r.y == r1.y + 1);
+                    if (!r5) continue;
+                    var r6 = robots.Skip(i+1).Any(r => r1.x + 2 == r.x && r.y == r1.y + 1);
+                    if (!r6) continue;
+                    var r7 = robots.Skip(i+1).Any(r => r1.x + 0 == r.x && r.y == r1.y + 2);
+                    if (!r7) continue;
+                    var r8 = robots.Skip(i+1).Any(r => r1.x + 1 == r.x && r.y == r1.y + 2);
+                    if (!r8) continue;
+                    var r9 = robots.Skip(i+1).Any(r => r1.x + 2 == r.x && r.y == r1.y + 2);
+                    if (!r9) continue;
+                    return s;
+                }
+            }
         }
 
         static (int x, int y, int vx, int vy)[] Parse(string[] lines)
