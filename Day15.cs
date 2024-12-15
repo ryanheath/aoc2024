9a626c454d1cd256983f98d09ec5443cff05cd21
Ryan Heath <ryan.q.heath@gmail.com>
2024-12-15 18:20:04 +0100

    small refactor

--- Day15.cs
+++ Day15.cs
@@ -8,7 +8,7 @@
 
         void ComputeExample()
         {
-            var input = 
+            var input =
                 """
                 ##########
                 #..O..O.O#
@@ -43,16 +43,16 @@
             Part2(input).Should().Be(1521952);
         }
 
-        int Part1(string[] lines) => GetSumGPS(RunInstructions(lines), 'O');
-        int Part2(string[] lines) => GetSumGPS(RunInstructionsScaleUp(lines), '[');
+        int Part1(string[] lines) => GetSumGPS(RunInstructions(lines));
+        int Part2(string[] lines) => GetSumGPS(RunInstructionsScaleUp(lines));
 
-        static int GetSumGPS(char[][] map, char search)
+        static int GetSumGPS(char[][] map)
         {
             var sum = 0;
 
             for (var y = 0; y < map.Length; y++)
             for (var x = 0; x < map[y].Length; x++)
-                if (map[y][x] == search)
+                if (map[y][x] is 'O' or '[')
                     sum += y * 100 + x;
 
             return sum;
@@ -64,21 +64,11 @@
 
             var (rx, ry) = GetRobot(map);
 
-            foreach (var m in instructions)
-            {
-                var (dx, dy) = m switch
-                {
-                    '<' => (-1, 0),
-                    '>' => (+1, 0),
-                    '^' => (0, -1),
-                    'v' or _  => (0, +1),
-                };
-                MoveRobot(dx, dy);
-            }
+            ExecuteInstructions(instructions, MoveRobot);
 
             return map;
 
-            void MoveRobot(int dx, int dy)
+            void MoveRobot(int dx, int dy, char _)
             {
                 var c = map[ry + dy][rx + dx];
                 if (c == '.')
@@ -87,19 +77,19 @@
                 }
                 else if (c == 'O')
                 {
-                    var (ddx, ddy) = (dx * 2, dy * 2);
+                    var (ddx, ddy) = (rx + dx * 2, ry + dy * 2);
 
-                    var dc = map[ry + ddy][rx + ddx];
-                    while (dc == 'O')
+                    c = map[ddy][ddx];
+                    while (c == 'O')
                     {
                         ddx += dx;
                         ddy += dy;
-                        dc = map[ry + ddy][rx + ddx];
+                        c = map[ddy][ddx];
                     }
-                    if (dc == '.')
+                    if (c == '.')
                     {
                         // move box(es)
-                        map[ry + ddy][rx + ddx] = 'O';
+                        map[ddy][ddx] = 'O';
 
                         DoMoveRobot();
                     }
@@ -123,17 +113,7 @@
 
             var (rx, ry) = GetRobot(map);
 
-            foreach (var m in instructions)
-            {
-                var (dx, dy) = m switch
-                {
-                    '<' => (-1, 0),
-                    '>' => (+1, 0),
-                    '^' => (0, -1),
-                    'v' or _  => (0, +1),
-                };
-                MoveRobot(dx, dy, m);
-            }
+            ExecuteInstructions(instructions, MoveRobot);
 
             return map;
 
@@ -162,39 +142,30 @@
                     while (checkCells.Count > 0)
                     {
                         var (boxx, boxy) = checkCells.Dequeue();
-                        if (m == '<')
+                        if (m is '<' or '>')
                         {
-                            var c = map[boxy][boxx - 1];
+                            var c = map[boxy][boxx + (m == '<' ? -1 : 2)];
                             if (c == '#') return false;
                             moveBoxes.Push((boxx, boxy));
-                            if (c == ']') checkCells.Enqueue((boxx - 2, boxy));
-                        }
-                        else if (m == '>')
-                        {
-                            var c = map[boxy][boxx + 2];
-                            if (c == '#') return false;
-                            moveBoxes.Push((boxx, boxy));
-                            if (c == '[') checkCells.Enqueue((boxx + 2, boxy));
+                            if (c == (m == '<' ? ']' : '[')) checkCells.Enqueue((boxx + (m == '<' ? -2 : 2), boxy));
                         }
                         else
                         {
                             var (c1, c2) = (map[boxy + dy][boxx], map[boxy + dy][boxx + 1]);
-                            if (c1 == '#' || c2 == '#') return false;
+                            if (c1 is '#' || c2 is '#') return false;
                             moveBoxes.Push((boxx, boxy));
-                            if (c1 == '[' && c2 == ']') checkCells.Enqueue((boxx, boxy + dy));
-                            if (c1 == ']') checkCells.Enqueue((boxx - 1, boxy + dy));
-                            if (c2 == '[') checkCells.Enqueue((boxx + 1, boxy + dy));
+                            if (c1 == '[') checkCells.Enqueue((boxx, boxy + dy)); // straight ahead
+                            if (c1 == ']') checkCells.Enqueue((boxx - 1, boxy + dy)); // left
+                            if (c2 == '[') checkCells.Enqueue((boxx + 1, boxy + dy)); // right
                         }
                     }
-                
+
                     // if we got here, we can move the boxes
                     while (moveBoxes.Count > 0)
                     {
                         var (boxx, boxy) = moveBoxes.Pop();
-                        map[boxy][boxx] = '.';
-                        map[boxy][boxx + 1] = '.';
-                        map[boxy + dy][boxx + dx] = '[';
-                        map[boxy + dy][boxx + dx + 1] = ']';
+                        (map[boxy]     [boxx],      map[boxy]     [boxx + 1])      = ('.', '.');
+                        (map[boxy + dy][boxx + dx], map[boxy + dy][boxx + dx + 1]) = ('[', ']');
                     }
 
                     return true;
@@ -208,6 +179,21 @@
                     rx += dx;
                     map[ry][rx] = '@';
                 }
+            }
+        }
+
+        static void ExecuteInstructions(string instructions, Action<int, int, char> moveRobot)
+        {
+            foreach (var m in instructions)
+            {
+                var (dx, dy) = m switch
+                {
+                    '<' => (-1, 0),
+                    '>' => (+1, 0),
+                    '^' => (0, -1),
+                    'v' or _  => (0, +1),
+                };
+                moveRobot(dx, dy, m);
             }
         }
 
@@ -229,17 +215,13 @@
                 var newLine = map[y] = new char[line.Length * 2];
 
                 for (var x = 0; x < line.Length; x++)
-                {
-                    var (c1, c2) = line[x] switch
+                    (newLine[x * 2], newLine[x * 2 + 1]) = line[x] switch
                     {
                         '#' => ('#', '#'),
                         '.' => ('.', '.'),
                         'O' => ('[', ']'),
                         '@' or _ => ('@', '.'),
                     };
-                    newLine[x * 2] = c1;
-                    newLine[x * 2 + 1] = c2;
-                }
             }
         }
 
@@ -247,16 +229,14 @@
         {
             var y = lines.Count(l => l.Length > 0 && l[0] == '#');
             var map = new char[y][];
+
             for (var i = 0; i < y; i++)
-            {
                 map[i] = lines[i].ToCharArray();
-            }
 
             var instructions = "";
+
             for (var i = y + 1; i < lines.Length; i++)
-            {
                 instructions += lines[i];
-            }
 
             return (map, instructions);
         }
